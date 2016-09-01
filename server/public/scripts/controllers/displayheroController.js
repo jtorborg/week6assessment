3b602ab17271b7a7aca1668cb785b3febcba5d69
Jacquelyn <jtorborg1227@comcast.net>
2016-09-01 12:26:12 -0500

    building array

--- server/public/scripts/controllers/displayheroController.js
+++ server/public/scripts/controllers/displayheroController.js
@@ -3,7 +3,21 @@ var myApp = angular.module("myApp", ["ngRoute"]);
 myApp.controller('IndexController', ['$scope', function($scope) {
   console.log("working!");
 
-  $scope.a = '  ';
-  $scope.b = '';
+  $scope.Invisibility = " ";
+  $scope.Flight = " ";
+  $scope.SuperSpeed = " ";
+  $scope.Flight = " ";
+  $scope.SuperSpeed = " ";
+  $scope.HeatVision = " ";
+  $scope.SuperStrength = " ";
+  $scope.AcceleratedHealing = " ";
+  $scope.PowerBlast = " ";
+  $scope.AnimalAffinity = " ";
+
+
+
+  $scope.superpowers = [$scope.Invisibility, $scope.Flight, $scope.SuperSpeed, $scope.HeatVision, $scope.SuperStrength, $scope.AcceleratedHealing, $scope.PowerBlast, $scope.AnimalAffinity
+  ];
+
 
 }]);
